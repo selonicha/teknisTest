f2ad69283ab023a39b0e82e1f0395f9a1e03da70
Muhammad Rafiq <mr.rafiudin02@gmail.com>
2022-12-14 15:46:13 +0700

    master

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function tampilanRegister()
+    {
+        return view('auth.register');
+    }
+    public function saveRegister(Request $request)
+    {
+        $request->validate([
+            'email'=> 'required',
+            'password'=>'required',
+            'name'=>'required'
+        ]);
+
+        $buatAkun=[
+            'email'=>$request->email,
+            'name'=>$request->name,
+            'password'=>Hash::make($request->password),
+        ];
+
+        User::create($buatAkun);
+
+        $cekAkun=[
+            'email'=>$request->email,
+            'password'=>$request->password,
+            'name'=>$request->name,
+        ];
+
+
+
+        if(Auth::attempt($cekAkun)){
+            return 'sukses';
+        } else {
+            return 'gagal';
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/login', [LoginController::class, 'tampilanLogin']);
+Route::get('/register', [RegisterController::class, 'tampilanRegister']);
+Route::post('/auth/register', [RegisterController::class, 'save']);
